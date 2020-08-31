d0596f31bad4dee650b4445bcf8d1cd7a609611c
Joey <1508224503@qq.com>
2020-08-31 18:23:18 +0800

    add build code

---
+++ build/precache-manifest.d79516f729da52a46ebaaead7d2a65e7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "229a27d348a252b02446ca25dbfb76fe",
+    "url": "/index.html"
+  },
+  {
+    "revision": "65c127aa098b7272c1e4",
+    "url": "/static/css/main.6580dbac.chunk.css"
+  },
+  {
+    "revision": "f767768f6430acfb64da",
+    "url": "/static/js/2.fc9ba48e.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.fc9ba48e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "65c127aa098b7272c1e4",
+    "url": "/static/js/main.6e4fdfbd.chunk.js"
+  },
+  {
+    "revision": "8cab146f387c6fa6faf4",
+    "url": "/static/js/runtime-main.03d328f6.js"
+  }
+]);
